1011755547fd68f94db9a675727562f517426cb0
Yulany Munevar <ddb_validator@apple.com>
2024-11-18 16:53:48 -0500

    jest library installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
- // tailwind.config.js
- module.exports = {
-
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-   media: false, // or 'media' or 'class'
-   theme: {
-     extend: {
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        PlayfairDisplay: ["Shadows Into Light ", "cursive"],
+      },
       spacing: {
-        '16': '4.5rem',
-      }
-     },
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+        16: "4.5rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
