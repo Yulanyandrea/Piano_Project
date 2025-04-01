8d8b1325e8398bf693c6a10173b7be7233aecf5f
Yulany Munevar <ddb_validator@apple.com>
2025-04-01 15:50:14 -0500

    change URL to connect virtual piano

--- src/features/piano/pianoApi.tsx
+++ src/features/piano/pianoApi.tsx
@@ -1,9 +1,11 @@
-const URL = process.env.NEXT_PUBLIC_API_KEY
+const URL = process.env.NEXT_PUBLIC_API_KEY;
 
 export const getAllInstruments = async () => {
   try {
-    const response = await fetch('http://localhost:3000/api/pianoKey');
-    const data = await response.json(); 
+    const response = await fetch(
+      "https://piano-project-git-tailwind-yulanyandreas-projects.vercel.app/api/pianoKey"
+    );
+    const data = await response.json();
     return data;
   } catch (error) {
     console.log(error);
